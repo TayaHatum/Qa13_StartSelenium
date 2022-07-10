bb8617da27f67c3e7d85a9a4ce047a9f9bdf170c
TayaHatum <21taya21@gmail.com>
2022-07-10 22:42:56 +0300

    last change

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,2 +1,39 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class FirstClass {
+    WebDriver wd;
+
+    @BeforeMethod
+    public void preCondition(){
+    WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver","/Users/tayahatum/Downloads/chromedriver");
+        wd = new ChromeDriver();
+
+
+        //wd.get("https://selenium.dev"); // without history
+        wd.navigate().to("https://selenium.dev");  // with history
+       // wd.navigate().back();
+      //  wd.navigate().forward();
+       // wd.navigate().refresh();
+
+    }
+
+    @Test
+    public void loginSuccess(){
+        // open form login   ---> LoginTab + click
+        // fill email valid data  ----> email +click +clear+type email
+        //fill password valid data  -----> password + +click +clear+type password
+        //click login button  ----> login button +click
+
+        // Expected result = Actual result
+
+
+    }
+
+
+
 }
